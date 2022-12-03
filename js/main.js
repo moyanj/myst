fc215dbee2cd8e05033fb2c4370c4c6aa6e7a75c
moyan <88998238+moyanj@users.noreply.github.com>
2022-12-03 14:20:29 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
-function setCookie(cvalue) {
-	var cname = "search"
-	document.cookie = cname + "=" + cvalue + "; ";
+function setCookie(cname = "search", cvalue, exdays = 730) {
+	var d = new Date();
+	d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+	var expires = "expires=" + d.toGMTString();
+	document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+// 检测是否为IE浏览器
+function isIe() {
+	var i = navigator.userAgent.toLowerCase().indexOf("msie");
+	return i >= 0;
 }
 
 function getCookie(cname) {
@@ -14,30 +21,9 @@ function getCookie(cname) {
 	}
 	return "https://cn.bing.com/search?q=";
 }
-var bodyBgs = [];
-bodyBgs[0] = "https://img1.imgtp.com/2022/11/30/qdwHox8E.png";
-bodyBgs[1] = "https://img1.imgtp.com/2022/11/30/GBu8rZG3.png";
-bodyBgs[2] = "https://img1.imgtp.com/2022/11/30/XyTU39yX.png";
-bodyBgs[3] = "https://img1.imgtp.com/2022/11/30/VckYBrmK.png";
-bodyBgs[4] = "https://img1.imgtp.com/2022/11/30/0qDmBOwZ.png";
-bodyBgs[5] = "https://img1.imgtp.com/2022/11/30/sUTrp2o9.png";
-bodyBgs[6] = "https://img1.imgtp.com/2022/11/30/I6lzqMJz.png";
-bodyBgs[7] = "https://img1.imgtp.com/2022/11/30/q1f7uhZb.png";
-bodyBgs[8] = "https://img1.imgtp.com/2022/11/30/GBu8rZG3.png";
-bodyBgs[9] = "https://img1.imgtp.com/2022/11/30/CnHatrLc.png";
-bodyBgs[10] = "https://img1.imgtp.com/2022/11/30/8Oe0IHhb.png";
-bodyBgs[11] = "https://img1.imgtp.com/2022/11/30/IhhZ91ZF.png";
-bodyBgs[12] = "https://img1.imgtp.com/2022/11/30/AKwCPfV9.png";
-bodyBgs[13] = "https://img1.imgtp.com/2022/11/30/vH7ZJWOY.png";
-bodyBgs[14] = "https://img1.imgtp.com/2022/11/30/IsfaF156.png";
-bodyBgs[15] = "https://img1.imgtp.com/2022/11/30/YTPifMEr.png";
-// 生成随机数
-var randomBgIndex = Math.round(Math.random() * 15);
-//输出随机的背景图
-document.write('<style>body{background:url(' + bodyBgs[randomBgIndex] +
-	') no-repeat;background-size:cover;}</style>');
-console.log("选择了第" + randomBgIndex + "个");
-console.log("url为：" + bodyBgs[randomBgIndex])
+var backgroundurl = "https://www.dmoe.cc/random.php"
+document.write('<style>body{background:url(' + backgroundurl +') no-repeat;background-size:cover;}</style>');
+
 
 function getCurDate() {
 	var d = new Date();
