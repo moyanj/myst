835eae51430c16a3c86fbc08b761b1c7c931cf61
moyan <88998238+moyanj@users.noreply.github.com>
2022-12-09 16:37:35 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+function backgroundurl(url) {
+	bgurl(url)
+}
+
+function bgurl(background) {
+	console.log(background)
+	document.write('<style>body{background:url(' + background + ') no-repeat;background-size:cover;}</style>');
+}
+
 function setCookie(cname = "search", cvalue, exdays = 730) {
 	var d = new Date();
 	d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
@@ -22,7 +31,7 @@ function getCookie(cname) {
 	return "https://cn.bing.com/search?q=";
 }
 var backgroundurl = "https://www.dmoe.cc/random.php"
-document.write('<style>body{background:url(' + backgroundurl +') no-repeat;background-size:cover;}</style>');
+document.write('<style>body{background:url(' + backgroundurl + ') no-repeat;background-size:cover;}</style>');
 
 
 function getCurDate() {
